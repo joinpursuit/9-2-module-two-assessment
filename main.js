2f3c6bc97427ae6884798c3ef6b92a06b914e024
Wwright91 <wisdomwright-martin@pursuit.org>
2022-09-19 18:36:27 -0400

    layout complete

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+  const base_url = "https://ghibliapi.herokuapp.com/films";
+  const select = document.querySelector("#titles");
+
+  fetch(base_url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+
+      for (let d of data) {
+        const option = document.createElement("option");
+        option.setAttribute("value", d.id);
+        console.log(option.value);
+        option.textContent = d.title;
+        select.append(option);
+      }
+    })
+    .catch((err) => console.log(err));
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
