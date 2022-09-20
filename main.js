1a9768e8f515b6591a4c578381d38b5dfc1d5bcb
ayd823 <aydiaz823@gmail.com>
2022-09-19 20:01:39 -0400

    Movie titles added to Select menu

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
+  const ghibliFilms = "https://ghibliapi.herokuapp.com/films";
+
+  fetch(ghibliFilms)
+    .then((response) => response.json())
+    .then((json) => {
+      console.log(json);
+
+      // Add movie titles to select menu.
+
+      const titles = document.querySelector("#titles");
+
+      for (let i = 0; i < json.length; i++) {
+        const option = document.createElement("option");
+        const { id, title } = json[i];
+        option.value = id;
+        option.textContent = title;
+        titles.append(option);
+      }
+    });
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
