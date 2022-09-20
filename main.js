74720723513569583082067a28142e4fe2b1c57a
i-guerrero <isaacguerrero@pursuit.org>
2022-09-19 20:09:44 -0400

    Select Menu Complete

--- main.js
+++ main.js
@@ -1,14 +1,21 @@
-
-
-
-
-
-
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
- // Add code you want to run on page load here
- 
+  // Add code you want to run on page load here
+  fetch("https://ghibliapi.herokuapp.com/films")
+    .then((response) => response.json())
+    .then((films) => {
+      console.log(films);
+      console.log(films[0]);
+      console.log(films[0].title);
+      // Loop through films array
+      // create and add an option to select for each film
+      films.forEach((film) => {
+        const option = document.createElement("option");
+        option.innerText = film.title;
+        document.querySelector("#titles").append(option);
+      });
+    });
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
