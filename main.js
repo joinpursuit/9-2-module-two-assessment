234a5f83520923c888e5abf868943f11158c41ba
Wwright91 <wisdomwright-martin@pursuit.org>
2022-09-20 13:41:27 -0400

    Assessment Completed

--- main.js
+++ main.js
@@ -1,99 +1,106 @@
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
 function run() {
-  const base_url = "https://ghibliapi.herokuapp.com/films";
+  const base_url = "https://ghibliapi.herokuapp.com/films/";
+  const people_url = "https://ghibliapi.herokuapp.com/people";
   const select = document.querySelector("#titles");
   const display_info = document.querySelector("#display-info");
-//   const review_section = document.querySelector("section");
-    const ul = document.querySelector("ul");
-    const form = document.querySelector("form");
-    const reset_button = document.querySelector("#reset-reviews")
-    const people_button = document.querySelector("#show-people")
+  const ul = document.querySelector("ul");
+  const form = document.querySelector("form");
+  const reset_button = document.querySelector("#reset-reviews");
+  const people_button = document.querySelector("#show-people");
+  const ol = document.querySelector("ol");
 
   fetch(base_url)
     .then((res) => res.json())
     .then((data) => {
-      console.log(data);
+      createOptions(data);
 
-      for (let d of data) {
-        const option = document.createElement("option");
-        option.setAttribute("value", d.id);
-        // console.log(option.value);
-        option.textContent = d.title;
-        select.append(option);
-      }
-        
+      // event listener to add reviews
       form.addEventListener("submit", (event) => {
         event.preventDefault();
 
         if (select.value === "") {
           alert("Please select a movie first");
         } else {
-            
-            for (let d of data) {
-                if (d.id === select.value) {
-
-
-                           const review_details = event.target.review.value
-          console.log(review_details)
-
-          const li = document.createElement("li");
-
-          li.innerHTML = `<strong>${d.title}:</strong> ${review_details}`;
-                    ul.append(li);
-                    form.reset();
-                }
+          for (let d of data) {
+            if (d.id === select.value) {
+              const review_details = event.target.review.value;
+              const li = document.createElement("li");
+              li.innerHTML = `<strong>${d.title}:</strong> ${review_details}`;
+              ul.append(li);
+              form.reset();
             }
+          }
         }
-      }); 
-        
+      });
     })
     .catch((err) => console.log(err));
 
+  // function to create options
+  function createOptions(data) {
+    for (let d of data) {
+      const option = document.createElement("option");
+      option.setAttribute("value", d.id);
+      option.textContent = d.title;
 
-    select.addEventListener("change", (event) => {
-        event.preventDefault();
+      select.append(option);
+    }
+  }
 
-    fetch(base_url)
+  // event listener to get the names of people associated with the movie
+  people_button.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    fetch(people_url)
+      .then((res) => res.json())
+      .then((people) => {
+        let allNamesArr = [];
+        for (let person of people) {
+          if (person.films.includes(base_url + select.value)) {
+            allNamesArr.push(person.name);
+          }
+        }
+
+        for (let liName of allNamesArr) {
+          const li = document.createElement("li");
+          li.innerText = liName;
+          ol.append(li);
+        }
+      });
+  });
+
+  //event listener to display movie details based on changes made
+  select.addEventListener("change", (event) => {
+    event.preventDefault();
+    fetch(base_url + "/")
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-
         const movieTitle = document.createElement("h3");
-        // console.log(movieTitle);
-
         const releaseYear = document.createElement("p");
         const description = document.createElement("p");
 
         display_info.innerHTML = "";
+        ol.innerHTML = "";
 
         for (let d of data) {
           if (select.value === d.id) {
             movieTitle.innerText = d.title;
-            console.log(movieTitle);
             releaseYear.textContent = d.release_date;
             description.textContent = d.description;
-            // console.log(description);
             display_info.append(movieTitle, releaseYear, description);
           }
         }
+      })
+      .catch((err) => console.log(err));
+  });
 
-      });
-  })
-  .catch((err) => console.log(err));
-    
-    
-    reset_button.addEventListener("click", event => {
-    event.preventDefault()
-ul.innerHTML = ""
-})
-    
+  // event listener for reset button
+  reset_button.addEventListener("click", (event) => {
+    event.preventDefault();
+    ul.innerHTML = "";
+  });
 }
-
-
-
-
-
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
