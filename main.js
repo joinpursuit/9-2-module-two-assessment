00af400e0c88a05015943d9c1200f8a2189cf19c
SharukhGV <sharukhvelupillai@pursuit.org>
2022-09-19 20:15:56 -0400

    added option dropdown menu title functionality

--- main.js
+++ main.js
@@ -8,7 +8,35 @@
 
 function run() {
  // Add code you want to run on page load here
+
+let apiFETCH = "https://ghibliapi.herokuapp.com/films/"
+
+ fetch(apiFETCH)
+.then((response) => response.json()).then((data) => {
+
+    for(element of data){
+        console.log(element)
+let optionyay = document.createElement("option")
+optionyay.setAttribute("value", `${element.id}`)
+optionyay.textContent=`${element.title}`
+document.querySelector("#titles-select").prepend(optionyay)
+
+    }
+
+
+
+})
+
+
+
+
+
+.catch((error) => {
+   
+    console.log(error)
+  }) 
 }
+
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
