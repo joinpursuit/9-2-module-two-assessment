26f8fd7352b78607fb73316a15228c8b6dc3d5e0
Bo Latt <bolattny@gmail.com>
2022-09-20 19:03:28 -0400

    fix people display on select change

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 const filmsURL = "https://ghibliapi.herokuapp.com/films";
 const select = document.querySelector("#titles");
 
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
-
 function run() {
-  // Add code you want to run on page load here
-
   fetch(filmsURL)
     .then((res) => res.json())
     .then((films) => injectOptionElements(films))
@@ -25,6 +21,7 @@ function injectOptionElements(films) {
 // select feature
 function handleSelectValueChange() {
   select.addEventListener("change", (e) => {
+    document.querySelector("#peopleList").innerHTML = "";
     console.log(e.target.value);
     const filmID = e.target.value;
     const filmByID = `https://ghibliapi.herokuapp.com/films/${filmID}`;
@@ -112,8 +109,5 @@ function handleShowPeople() {
   });
 }
 handleShowPeople();
-// This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
-// So that testing can work as expected for now
-// A non-hacky solution is being researched
 
 setTimeout(run, 1000);
