b88f41045fb1b701fe0933987d9832561d78cacf
cjs2408 <carlossuero@pursuit.org>
2022-09-20 19:28:49 -0500

    Html, Javasc,CSS fixes 22

--- main.js
+++ main.js
@@ -1,174 +1,100 @@
-const filmsURL = "https://ghibliapi.herokuapp.com/films";
-const select = document.querySelector("#titles");
-
-
-
-
-
-// To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
-
 function run() {
+  // Add code you want to run on page load here
+  const baseUrl = "https://ghibliapi.herokuapp.com/films";
+  const urlPeople = "https://ghibliapi.herokuapp.com/people";
+  const title = document.querySelector("#titles");
+  const button = document.querySelector("#show-people");
+  const form = document.querySelector("form");
+  const ul = document.querySelector("ul");
+  const ol = document.querySelector("ol");
+  const reviewReset = document.querySelector("#reset-reviews");
 
-
-
-fetch(filmsURL)
-.then((res) => res.json())
-.then((films) => injectOptionElements(films))
-.catch((err) => console.log(err));
-}
-
-function injectOptionElements(films) {
-console.log(films);
-for (let film of films) {
-const option = document.createElement("option");
-option.textContent = film.title;
-option.setAttribute("value", film.id);
-select.append(option);
-}
-}
-
-// select feature
-function handleSelectValueChange() {
-select.addEventListener("change", (e) => {
-console.log(e.target.value);
-const filmID = e.target.value;
-const filmByID = `https://ghibliapi.herokuapp.com/films/${filmID}`;
-fetch(filmByID)
-  .then((res) => res.json())
-  .then((film) => showMovieDetails(film))
-  .catch((err) => console.log(err));
-});
-}
-handleSelectValueChange();
-
-function showMovieDetails(film) {
-console.log(film);
-const displayDiv = document.querySelector("#display-info");
-displayDiv.innerHTML = "";
-const p1 = document.createElement("p");
-const p2 = document.createElement("p");
-const p3 = document.createElement("p");
-p1.textContent = film.title;
-p1.classList.add("movie-title");
-p2.textContent = film.release_date;
-p3.textContent = film.description;
-
-displayDiv.append(p1, p2, p3);
-}
-
-function handleReviewSubmit() {
-const reviewForm = document.querySelector("#review-form");
-reviewForm.addEventListener("submit", (e) => {
-e.preventDefault();
-// if no movie is selected, alert
-if (!select.value) {
-  alert("Please select a movie first");
-  return;
-}
-
-// if selected, get user input
-console.log(e.target.review.value);
-const review = e.target.review.value;
-e.target.review.value = "";
-
-// then show review
-const reviewList = document.querySelector(".reviews-list");
-const li = document.createElement("li");
-const strong = document.createElement("strong");
-strong.textContent = document.querySelector(".movie-title").innerText + ":";
-li.innerText = " " + review;
-li.prepend(strong);
-reviewList.append(li);
-});
-}
-handleReviewSubmit();
-
-function handleResetReviews() {
-const deleteBtn = document.querySelector("#reset-reviews");
-deleteBtn.addEventListener("click", () => {
-const reviewsList = document.querySelector(".reviews-list");
-reviewsList.innerHTML = "";
-});
-}
-handleResetReviews();
-
-function handleShowPeople() {
-const peopleUrl = "https://ghibliapi.herokuapp.com/people";
-const showPeopleBtn = document.querySelector("#show-people");
-showPeopleBtn.addEventListener("click", () => {
-//get all the people, filter people by current movid id
-fetch(peopleUrl)
-  .then((res) => res.json())
-  .then((allPeople) => {
-    console.log("allPeople", allPeople);
-    const filmID = select.value;
-    console.log(filmID);
-    const filteredPeople = allPeople.filter((person) =>
-      person.films.some((film) => film.split("/").slice(-1)[0] == filmID)
-    );
-    console.log(filteredPeople);
-    const peopleList = document.querySelector("#peopleList");
-    for (let person of filteredPeople) {
-      const li = document.createElement("li");
-      li.textContent = person.name;
-      peopleList.append(li);
-    }
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    ol.innerHTML = "";
+    fetch(urlPeople)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        if (!title.value) {
+          window.alert("Please select a movie first");
+        } else {
+          json.forEach((element) => {
+            const personTag = document.createElement("li");
+            personTag.textContent = element.name;
+            ol.append(personTag);
+          });
+        }
+      })
+      .catch((err)=> console.log(err));
   });
-});
+
+  // adds movie titles to dropdown
+  fetch(baseUrl)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      for (let films of json) {
+        const option = document.createElement("option");
+        option.setAttribute("value", films.id);
+        option.textContent = films.title;
+        title.append(option);
+      }
+      
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (!title.value) {
+          window.alert("Please select a movie first");
+          // const userInput = e.target.review.value;
+        } else {
+          for (let films of json) {
+            if (films.id === title.value) {
+              const userInput = e.target.review.value;
+              const li = document.createElement("li");
+              li.innerHTML = `<strong>${films.title}:</strong> ${userInput}`;
+              ul.append(li);
+            }
+          }
+          form.reset();
+        }
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  title.addEventListener("change", (e) => {
+    e.preventDefault();
+
+    fetch(baseUrl)
+      .then((res) => res.json())
+      .then((json) => {
+        const displayInfoDiv = document.querySelector("#display-info");
+        displayInfoDiv.innerHTML = "";
+        for (let films of json) {
+          if (films.id === title.value) {
+            const h3 = document.createElement("h3");
+            const ptagYear = document.createElement("p");
+            const ptagDescription = document.createElement("p");
+            h3.textContent = films.title;
+            ptagYear.textContent = films.release_date;
+            ptagDescription.textContent = films.description;
+            displayInfoDiv.append(h3, ptagYear, ptagDescription);
+          }
+        }
+      })
+      .catch((err)=>console.log(err));
+  });
+
+  
+  reviewReset.addEventListener("click", (e) => {
+    e.preventDefault();
+    ul.innerHTML = "";
+  });
 }
-
- // Add code you want to run on page load here
-
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
 // So that testing can work as expected for now
 // A non-hacky solution is being researched
 
-// setTimeout(run, 1000);
-
-// const movies = "https://ghibliapi.herokuapp.com";
-
-// fetch(movies)
-//     .then(response => response.json())
-//     .then(renderMoviesWithCreate)
- 
-// function renderMoviesWithInnerHtml(json) {
-//     const main = document.querySelector("main");
-//     main.innerHTML = "";
-//     json.forEach(movies => {
-//         const div = `<div class="movies">
-//         <a href="${movies.website_url || "#"}">
-//                 <h2>${movies.name}</h2>;
-//             <a/>
-//         </div>`;
-//         main.innerHTML += div;
-//     })
-// }
-
-// function renderMoviesWithCreate(json) {
-//     const main = document.querySelector("main");
-//     main.innerHTML = "";
-
-//     json.forEach(movies => {
-//         const div = document.createElement("div");
-//         div.classList.add("movies");
-
-//         const a = document.createElement("a");
-//         a.setAttribute("href", movies.website_url || "#");
-
-//         const h2 = document.createElement("h2");
-//         h2.textContent = movies.name;
-
-//         const p = document.createElement("p");
-//         const p2 = document.createElement("p");
-//         const p3 = document.createElement("p");
-
-//         div.prepend(a, p, p2, p3);
-//         a.prepend(h2);
-
-//         p.textContent = movies.name;
-        
-
-//         main.append(div);
-//     })
-// }
+setTimeout(run, 1000);
