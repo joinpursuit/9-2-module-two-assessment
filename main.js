4a07ea43a626e41171cf59276baeb67110a2e22d
Karyn Huston <huston.karyn@gmail.com>
2022-09-20 21:22:30 -0400

    initial commit

--- main.js
+++ main.js
@@ -7,12 +7,55 @@ function selectionMenu(json) {
   options.textContent = `${movieT.title}`
   select.append(options)
    });
+}
 
-   const movieDetails(json) {
-const displayDetails = document.querySelector("#display-info")
-   }
+
+const films = "https://ghibliapi.herokuapp.com/films";
+
+fetch(films)
+    .then(response => response.json())
+    .then(display)
+
+function display(json) {
+  const select = document.querySelector("#titles");
+  const display = document.querySelector("#display-info")
+ 
+  select.addEventListener("change", (event) => {
+    event.preventDefault();
+    const userSelected = event.target.value;
+    console.log(userSelected)
+    display.textContent = ""
+    const title = document.createElement("h3")
+    title.textContent = `${userSelected}`
+      let j = json.find(m => m.title ===  userSelected)
+  console.log(j)
+const releaseYear = document.createElement("p")
+const description = document.createElement("p")
+releaseYear.textContent = j.release_date
+ description.textContent = j.description
+ display.append(title,releaseYear,description)
+
+})
 
 }
+
+
+
+function getReview(json) {
+  const review = document.querySelector("#review")
+  review.addEventListener("submit", (event) => {
+event.preventDefault()
+    const userValue = event.target.value
+  const ol = document.createElement("ol")
+    let j = json.find(m => m.title === userValue)
+    ol += `<li><strong>${j.title}</strong></li>
+    <li>${userValue}</li>`
+    review.append(ol)
+    review.reset()
+  })
+}
+
+ 
 
 // To ensure Cypress tests work as expeded, add any code/functions that you would like to run on page load inside this function
 
@@ -22,7 +65,7 @@ function run() {
 
   fetch(films)
     .then((response) => response.json())
-    .then(selectionMenu, movieDetails);
+    .then(selectionMenu,display,getReview);
 }
 
 // This function will "pause" the functionality expected on load long enough to allow Cypress to fully load
